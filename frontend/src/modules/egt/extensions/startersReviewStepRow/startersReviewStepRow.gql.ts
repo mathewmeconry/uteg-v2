d02d08906c7d4d157141ae86a8c695be4c2ccc30
Mathias Scherer <scherer.mat@gmail.com>
2024-02-18 17:10:10 +0100

    fix(egt): dummy export to make vite happy

--- frontend/src/modules/egt/extensions/startersReviewStepRow/startersReviewStepRow.gql.ts
+++ frontend/src/modules/egt/extensions/startersReviewStepRow/startersReviewStepRow.gql.ts
@@ -1,6 +1,6 @@
 import gql from "graphql-tag";
 
-const DIVISION_QUERY = gql`
+export const DIVISION_QUERY = gql`
     query StartersReviewStepRowDivisions($filter: EGTDivisionFilterInput!) {
         egtDivisions(filter: $filter) {
             id
