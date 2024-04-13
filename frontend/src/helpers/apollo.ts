00d90fb9126c78f6e1fbe970af46bcd80997ed76
Mathias Scherer <scherer.mat@gmail.com>
2024-04-13 13:20:12 +0200

    fix(frontend): login bug where token isn't set

--- frontend/src/helpers/apollo.ts
+++ frontend/src/helpers/apollo.ts
@@ -1,13 +1,21 @@
-import { ApolloClient, InMemoryCache, split } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  operationName,
+  split,
+} from "@apollo/client";
 import { isTokenValid } from "./auth";
 import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
 import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
 import { createClient } from "graphql-ws";
 import { getMainDefinition } from "@apollo/client/utilities";
 
-const token = localStorage.getItem("token");
-if (!isTokenValid()) {
-  console.log("INVALID!");
+function getToken() {
+  const token = localStorage.getItem("token");
+  if (token && isTokenValid()) {
+    return `Bearer ${token}`;
+  }
+  return "";
 }
 
 const backend = import.meta.env.VITE_BACKEND_URI ?? document.location.origin;
@@ -16,7 +24,6 @@ const uploadLink = createUploadLink({
   uri: `${backend}/graphql`,
   headers: {
     "Apollo-Require-Preflight": "true",
-    authorization: token ? `Bearer ${token}` : "",
   },
 });
 
@@ -25,15 +32,18 @@ const wsLink = new GraphQLWsLink(
     url: `${backend}/graphql`
       .replace("https://", "wss://")
       .replace("http://", "ws://"),
-    connectionParams: {
-      Authorization: token ? `Bearer ${token}` : "",
-    },
   })
 );
 
 const splitLink = split(
   (op) => {
     const definition = getMainDefinition(op.query);
+    op.setContext(({ headers = {} }) => ({
+      headers: {
+        ...headers,
+        Authorization: getToken(),
+      },
+    }));
     return (
       definition.kind === "OperationDefinition" &&
       definition.operation === "subscription"
