011c13397b74f5d65ea425a1e0e8312ea9038b4f
Mathias Scherer <scherer.mat@gmail.com>
2024-04-13 14:08:05 +0200

    fix(frontend): adds missing ID to competitionlayout query

--- frontend/src/layouts/competitionlayout.gql.ts
+++ frontend/src/layouts/competitionlayout.gql.ts
@@ -3,6 +3,7 @@ import gql from "graphql-tag";
 export const COMPETITION_NAME = gql`
     query competitionName($id: ID!) {
         competition(id: $id) {
+            id
             name
         }
     }
